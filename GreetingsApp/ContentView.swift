15016aa92b299abf937567b60d07d59717b57874
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-10-24 23:40:36 +0300

    Initial Commit

---
+++ GreetingsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreetingsApp
+//
+//  Created by MIKHAIL ZHACHKO on 24.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreetingsApp/GreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsAppApp.swift
+//  GreetingsApp
+//
+//  Created by MIKHAIL ZHACHKO on 24.10.23.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
