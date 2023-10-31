19beb5fd12b23ce33aec2359a17d4e19b479850d
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-10-31 23:55:12 +0300

    refactor: change contextMenu to menu => old button with long tap to new button with single short tap

--- GreetingsApp/Views/LanguageOptionsView.swift
+++ GreetingsApp/Views/LanguageOptionsView.swift
@@ -12,21 +12,25 @@ struct LanguageOptionsView: View {
     @Binding var layoutDirectionString: String
     
     var body: some View {
-        Image(systemName: "globe")
-            .contextMenu{
-                Button(LocalizedStringKey("English")) {
-                    language = "en"
-                    layoutDirectionString = LEFT_TO_RIGHT
-                }
-                Button(LocalizedStringKey("Russian")) {
-                    language = "ru"
-                    layoutDirectionString = LEFT_TO_RIGHT
-                }
-                Button(LocalizedStringKey("Belarusian")) {
-                    language = "be-BY"
-                    layoutDirectionString = LEFT_TO_RIGHT
-                }
+        
+        Menu {
+            Button(LocalizedStringKey("English")) {
+                language = "en"
+                layoutDirectionString = LEFT_TO_RIGHT
             }
+            Button(LocalizedStringKey("Russian")) {
+                language = "ru"
+                layoutDirectionString = LEFT_TO_RIGHT
+            }
+            Button(LocalizedStringKey("Belarusian")) {
+                language = "be-BY"
+                layoutDirectionString = LEFT_TO_RIGHT
+            }
+        } label: {
+            Image(systemName: "globe")
+                .font(.system(size: 20))
+                .foregroundColor(.black)
+        }
     }
 }
 
